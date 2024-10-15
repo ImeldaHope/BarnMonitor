1ef434cf19d02800c62c276adfc9ac71a07932e9
Imelda Hope <imeldalarusso@gmail.com>
2024-10-15 10:36:41 +0300

    creates config data

---
+++ server/config.py
@@ -0,0 +1,33 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.orm import validates
+from sqlalchemy import MetaData
+
+from flask import Flask, jsonify, request, make_response
+from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
+from flask_restful import Api, Resource
+from sqlalchemy.exc import IntegrityError
+
+convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
+metadata=MetaData(naming_convention=convention)
+db=SQLAlchemy(metadata=metadata)
+
+app=Flask(__name__)
+app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+migrate=Migrate(app, db)
+db.init_app(app)
+
+bcrypt=Bcrypt(app)
+api=Api(app)
